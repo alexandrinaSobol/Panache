a42dbecf6c4650699c9a08b40497b577ab413bd7
a.sobol <alexandrina.sobol@isa.utm.md>
2021-03-15 19:03:10 +0200

    add aplication.proprietes

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO fruits(id, name, color) VALUES (nextval('hibernate_sequence'), 'Cherry', 'red');
+INSERT INTO fruits(id, name, color) VALUES (nextval('hibernate_sequence'), 'Apple', 'red');
+INSERT INTO fruits(id, name, color) VALUES (nextval('hibernate_sequence'), 'Banana', 'red');
